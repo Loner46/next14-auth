887cc6d4ae920f2f029a447b890722a73f49c455
Makhmudov Alisher <m.alisher.1996@gmail.com>
2024-02-17 21:57:18 +0900

    04. Adding domain to a mail system

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "auth@alisher.asia",
     to: email,
     subject: "Confirm your email",
     html: `<p>Your 2FA code: ${token}!</p>`,
@@ -16,7 +16,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "auth@alisher.asia",
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm your email!</p>`,
@@ -27,7 +27,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "auth@alisher.asia",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${confirmLink}">here</a> to reset your password!</p>`,
